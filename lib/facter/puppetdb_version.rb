f2c9214ec502d2c063727d98044c83978d314ac5
Steve Mokris <steve@kosada.com>
2024-06-28 20:29:19 -0400

    Support extracting version from Debian's puppetdb wrapper.

--- lib/facter/puppetdb_version.rb
+++ lib/facter/puppetdb_version.rb
@@ -2,7 +2,17 @@ Facter.add(:puppetdb_version) do
   confine { Facter::Util::Resolution.which('puppetdb') }
 
   setcode do
-    output = Facter::Core::Execution.execute('puppetdb --version')
-    output.split(':').last.strip
+    if Facter.value(:os)['family'] == 'Debian'
+      output = Facter::Core::Execution.execute('puppetdb version')
+      # example output:
+      # version=8.4.1
+      # target_schema_version=84
+      output.split("\n").first.split('=').last
+    else
+      output = Facter::Core::Execution.execute('puppetdb --version')
+      # example output:
+      # puppetdb version: 7.18.0
+      output.split(':').last.strip
+    end
   end
 end
